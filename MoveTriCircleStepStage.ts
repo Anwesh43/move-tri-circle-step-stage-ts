e05555a9dde5eae06398f974ed6f4616692ba88d
Anwesh43 <anweshthecool0@gmail.com>
2018-12-16 23:46:57 +0530

    adding MTCNode class

--- MoveTriCircleStepStage.ts
+++ MoveTriCircleStepStage.ts
@@ -114,3 +114,46 @@ class Animator {
         }
     }
  }
+
+ class MTCNode {
+    state : State = new State()
+    prev : MTCNode
+    next : MTCNode
+
+    constructor(private i : number) {
+        this.addNeighbor()
+    }
+    addNeighbor() {
+        if (this.i < nodes - 1) {
+            this.next = new MTCNode(this.i + 1)
+            this.next.prev = this
+        }
+    }
+
+    draw(context : CanvasRenderingContext2D) {
+        drawMTCNode(context, this.i, this.state.scale)
+        if (this.next) {
+            this.next.draw(context)
+        }
+    }
+
+    update(cb : Function) {
+        this.state.update(cb)
+    }
+
+    startUpdating(cb : Function) {
+        this.state.startUpdating(cb)
+    }
+
+    getNext(dir : number, cb : Function) : MTCNode {
+        var curr : MTCNode = this.prev
+        if (dir == 1) {
+            curr = this.next
+        }
+        if (curr) {
+            return curr
+        }
+        cb()
+        return this 
+    }
+ }
