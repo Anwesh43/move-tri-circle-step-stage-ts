f093172884c699576a087e1711c6ef402dba7910
Anwesh43 <anweshthecool0@gmail.com>
2018-12-16 23:51:36 +0530

    adding MoveTriCircleStep class

--- MoveTriCircleStepStage.ts
+++ MoveTriCircleStepStage.ts
@@ -154,6 +154,29 @@ class Animator {
             return curr
         }
         cb()
-        return this 
+        return this
+    }
+ }
+
+ class MoveTriCircleStep {
+    root : MTCNode = new MTCNode(0)
+    curr : MTCNode = this.root
+    dir : number = 1
+
+    draw(context : CanvasRenderingContext2D) {
+        this.root.draw(context)
+    }
+
+    update(cb : Function) {
+        this.curr.update(() => {
+            this.curr = this.curr.getNext(this.dir, () => {
+                this.dir *= -1
+            })
+            cb()
+        })
+    }
+
+    startUpdating(cb : Function) {
+        this.curr.startUpdating(cb)
     }
  }
